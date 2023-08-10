b33530399b5ab47e04913597f63e6f1f4610acc0
nrtimms <nataliertimms18@gmail.com>
2023-08-09 17:33:29 -0700

    connected SQL database

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
+//Dependencies
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const { Sequelize } = require('sequelize')
 
+//Configuration
+require('dotenv').config()
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+//Sequelize Connection
+const sequelize = new Sequelize(process.env.PG_URI)
+
+try {
+    sequelize.authenticate() 
+    console.log(`Connected with Sequelize at ${process.env.PG_URI}`) 
+} catch(err) {
+    console.log(`Unable to connect to PG: ${err}`) 
+}
+
+// Root
 app.get('/', (req, res) => {
   res.send('Hello, Express!');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+// Listen
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on http://localhost:${process.env.PORT}`);
 });
