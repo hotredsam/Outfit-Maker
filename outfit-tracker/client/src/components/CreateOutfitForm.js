8b9961499d8b1012d55f944aafdfc6fff51fef1d
Nalinaka <nalinaka@gmail.com>
2023-08-16 16:59:48 -0700

    Debugged and page is now loading, sit works and view outfits page can be viewed

--- outfit-tracker/client/src/components/CreateOutfitForm.js
+++ outfit-tracker/client/src/components/CreateOutfitForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { createOutfit } from '../actions/outfitActions'; // Create this action
-import { createAccount } from '../components/accountActions'; // Adjust this path if necessary
+// import { createAccount } from '../components/accountActions'; // Adjust this path if necessary
 
 function CreateOutfitForm() {
   const [outfitName, setOutfitName] = useState('');
