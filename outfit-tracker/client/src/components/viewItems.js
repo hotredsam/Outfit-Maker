b70a2ed145e70b16f1bb40d903bfa0c8792f2493
Nicolas Nueca <nicolas572@yahoo.com>
2023-08-16 12:59:52 -0700

    adding container to handle image uploads

--- outfit-tracker/client/src/components/viewItems.js
+++ outfit-tracker/client/src/components/viewItems.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import './viewItems.css';
+import './ScrollableSection.css';
+import ScrollableSection from './ScrollableSection';
 
 const ViewItems = () => {
+    const images = [
+        'https://picsum.photos/200',
+        'https://picsum.photos/200',
+        'https://picsum.photos/200',
+        'https://picsum.photos/200',
+        'https://picsum.photos/200',
+        'https://picsum.photos/200',
+    ].map(s => ({
+        top: s,
+        middle: s,
+        bottom: s
+    }));
     
     return (
         <div className='view-items-content'>
             <h1>View Items</h1>
-
+            <div className="scrollable-section-container">
+                <ScrollableSection images={images} />
+            </div>
 
         </div>
     );
